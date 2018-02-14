ab81126a10c439b2da32ef1e02e33b764b84cea5
Francis Redido <sicnarfodider@hotmail.com>
2018-02-13 18:13:05 -0800

    initial client setup

---
+++ client/src/components/landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LandingPage{
+    render(){
+        return(
+            <div>
+                <h1>this is land</h1>
+            </div>
+        )
+    }
+}
+
+export default LandingPage;
